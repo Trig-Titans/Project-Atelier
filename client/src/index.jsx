010d5ae8363db97ded2c63ecbcc71566e3a3f570
Nick Adam <na4hx@virginia.edu>
2022-08-25 16:29:50 -0400

    Added a component variable in QuestionSearch and imported it to index file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
+import QuestionSearch from './components/questions/QuestionSearch.jsx'
 const root = createRoot(document.getElementById("root"));
 
 
