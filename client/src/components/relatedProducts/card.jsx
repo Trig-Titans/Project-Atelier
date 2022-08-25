4d15325d8185129c71bd8003bdb4f464766c582a
Christopher Garcia <chrisgood2go@gmail.com>
2022-08-25 16:28:06 -0400

    attempt a merg conflict with the card component in related products folder

---
+++ client/src/components/relatedProducts/card.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Card = (props) => {
+
+  return (
+    <h1>Card Component</h1>
+  )
+}
+
+export default Card;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
+import Card from './components/relatedProducts/card.jsx'
 const root = createRoot(document.getElementById("root"));
 
 
@@ -8,7 +9,10 @@ const App = () => {
             <h1>Front End Capstone Avatar Project</h1>
               <div>Overview Div</div>
               <div>Questions Div</div>
-              <div>Related Div</div>
+              <div>
+                <p>related Div</p>
+                <Card/>
+              </div>
               <div>Reviews Div</div>
           </div>
 }
