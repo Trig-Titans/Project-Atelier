8738ebc5230eb48c7d46f1fbe766bb48f38bdcde
Christopher Garcia <chrisgood2go@gmail.com>
2022-08-26 10:28:35 -0400

    add card component

--- client/src/components/relatedProducts/card.jsx
+++ client/src/components/relatedProducts/card.jsx
@@ -2,27 +2,36 @@ import React from 'react';
 import axios from 'axios';
 
 const cardStyle = {
-  /* Add shadows to create the "card" effect */
-  "box-shadow": "0 4px 8px 0 rgba(0,0,0,0.2);",
-  transition: "0.3s;",
   border: "solid",
-  width: "max-content"
+  width: "200px",
+  height: "299px",
 }
 
-/* Add some padding inside the card container */
+const pic = {
+  width: "200px",
+  height: "195px"
+}
 const containerStyle = {
   padding: "2px 16px;",
+  position: "relative",
+  bottom: 0
 
+}
+
+const category_Price = {
+  "font-size": '11px'
 }
 
 const Card = (props) => {
+  axios
 
   return (
     <div class="card" style={cardStyle}>
-    <img src="https://image.shutterstock.com/image-vector/colorful-illustration-test-word-260nw-1438324490.jpg"/>
+    <img src="https://image.shutterstock.com/image-vector/colorful-illustration-test-word-260nw-1438324490.jpg" style={pic}/>
       <div class="container" style={containerStyle}>
-        <h4><b>John Doe</b></h4>
-        <p>Architect & Engineer</p>
+      <p style={category_Price}>CATEGORY</p>
+        <h5><b>John Doe</b></h5>
+        <p style={category_Price}>$123</p>
       </div>
   </div>
   )
