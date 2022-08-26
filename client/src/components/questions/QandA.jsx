e5f827d2abee60df0fc4083e6b502beac10eed75
Nick Adam <na4hx@virginia.edu>
2022-08-26 12:39:11 -0400

    created api call for questions and gave state to QandA component

--- client/src/components/questions/QandA.jsx
+++ client/src/components/questions/QandA.jsx
@@ -1,10 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import QuestionSearch from './QuestionSearch.jsx';
 import QuestionList from './QuestionList.jsx';
+import axios from 'axios';
+import API_KEY from '../../../../config.js';
 
-const QandA = (props) => {
+const QandA = () => {
 
   //depending on what product is given as prop make api request and change state
+  var product_id = 37314;
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions?product_id=${product_id}`,
+      headers: {Authorization: API_KEY}
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  })
+
 
   return (
     <div>
