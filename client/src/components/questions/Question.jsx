245d99d2207a9415ef34fde8b68cd33191a2dbe4
Nick Adam <na4hx@virginia.edu>
2022-08-26 16:18:21 -0400

    successfully displayed answers for each question in the correct formatting

--- client/src/components/questions/Question.jsx
+++ client/src/components/questions/Question.jsx
@@ -1,10 +1,41 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledQuestion = styled.div`
+  text-align: left;
+  //padding: 0.7% 0;
+`;
+const StyledAnswer = styled.div`
+  padding: 0.7% 0;
+`;
 
 const Question = ({ questionData }) => {
+
+  // turns the answers object into an array for easy iteration
+  var answerArray = [];
+  for (var key in questionData.answers) {
+    answerArray.push(questionData.answers[key]);
+  }
+
   return (
-    <div>I am a question</div>
+    <StyledQuestion >
+      <strong>{'Q: ' + questionData.question_body}</strong>
+      <div>
+        {answerArray.map((answer) => {
+          if (answer === answerArray[0]) {
+            return (
+            <StyledAnswer key={answer.id}><strong>A: </strong>{answer.body}</StyledAnswer>
+            )
+          }
+          return (
+            <StyledAnswer key={answer.id}><strong style={{visibility: 'hidden'}}>A: </strong>{answer.body}</StyledAnswer>
+          )
+        })}
+      </div>
+    </StyledQuestion>
+
   )
 }
 
