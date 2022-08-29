ae7a2ca05b0d253faefe5c1919fd0365df667314
Cathy Mosinski <jcmosinski@gmail.com>
2022-08-29 10:11:35 -0400

    organizing components

--- client/src/components/reviews/reviewListComponents/ReviewsList.jsx
+++ client/src/components/reviews/reviewListComponents/ReviewsList.jsx
@@ -1,38 +1,17 @@
 import React from 'react';
-import {Checkmark} from 'react-checkmark';
-import {ReviewButtons} from './AdditionalButtons.jsx'
-
-
-
-
-let Sorter = () => {
-  return (<div>### of reviews, sorted by DropDownList</div>);
-}
-
-let ReviewTile = () => {
-  return (<div>
-    <p> Star Rating  Username and Date</p>
-    <p>Title Line - summary max 60 char and bold</p>
-    <p>Message Body</p>
-    <p>Thumbnail pictures if any</p>
-    <p><Checkmark size='medium'/> I recommend this product! -- make it a banner?</p>
-    <p>Response from Seller: if any and message attached. Styling should be different</p>
-    <p>Is this helpful? Yes [###] | No [###]</p>
-    <hr />
-  </div>);
-}
-
-
-
+import {ReviewButtons} from './AdditionalButtons.jsx';
+import {ReviewTiles} from './ReviewTiles.jsx';
+import {SorterBar} from './SorterBar.jsx';
+import {ReviewsContainer} from '../sharedStyles/sharedStyledComponents';
 
 let ReviewsList = ( {reviews} ) => {
   let reviewList = reviews.results;
-  return (<div>
+  return (<ReviewsContainer>
     <p>{console.log(reviews)}</p>
-    <Sorter />
-    <ReviewTile />
+    <SorterBar />
+    <ReviewTiles reviewList={reviewList} />
     <ReviewButtons />
-    </div>)
+    </ReviewsContainer>)
 }
 
 export default ReviewsList;
