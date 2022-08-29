40b706bb3beea353c78e3e42db100f42d7912071
Nick Adam <na4hx@virginia.edu>
2022-08-29 15:45:33 -0400

    adds functionality to load more answers if answer list is greater than 2 in length

--- client/src/components/questions/AnswerList.jsx
+++ client/src/components/questions/AnswerList.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Answer } from './Answer.jsx';
 import styled from 'styled-components';
 
@@ -10,16 +10,32 @@ const StyledAnswerList = styled.div`
 
 const AnswerList = ({ answers }) => {
 
+  const [loadMore, setLoadMore] = useState('none');
+  const [list, setList] = useState(answers);
+
+  useEffect(() => {
+    if (answers.length > 2) {
+      setLoadMore('block');
+      setList(list.slice(0,2));
+    }
+  }, [])
+
+  var loadAnswers = () => {
+    setList(answers);
+    setLoadMore('none');
+  }
+
   return (
     <StyledAnswerList>
       <div>
           <strong>A:&nbsp;   </strong>
       </div>
       <div>
-        {answers.map((answer) => {
+        {list.map((answer) => {
           return (
           <Answer answerData={answer} key={answer.id} />
           )})}
+        <strong style={{display: loadMore}} onClick={loadAnswers}>See more answers</strong>
       </div>
     </StyledAnswerList>
   )
