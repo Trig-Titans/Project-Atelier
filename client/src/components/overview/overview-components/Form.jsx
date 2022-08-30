76485699b1340fdc7644a81ce4328fd4041be31d
Nick Kozlarek <kozlarek.nicholas@gmail.com>
2022-08-30 08:54:17 -0600

    Implement quantity drop down change on when choosing sizes

--- client/src/components/overview/overview-components/Form.jsx
+++ client/src/components/overview/overview-components/Form.jsx
@@ -9,25 +9,49 @@ const StyledOverviewFavoriteStar = styled.button`
 
 export default function OverViewForm({ styles }) {
   const sizeAndQuant = Object.values(styles.skus);
+  var [currentSize, setSize] = useState(0);
+  console.log(sizeAndQuant);
+  // create an array of numbers containing all of the quantities
+  function createQuantArray(quantity) {
+    var array = [];
+    for (var i = 0; i < quantity; i ++) {
+      array.push(i + 1);
+    }
+    return array;
+  }
+
+  // make a variable using the function above
+  const quantArray = createQuantArray(sizeAndQuant[currentSize].quantity);
+
+  // map out the sizes into the options bar
   const sizeList = sizeAndQuant.map((size, index) => {
     return (
-      <option value={size.size} key={index}>{size.size}</option>
+      <option value={index} key={index}>{size.size}</option>
     )
   })
+
+  // Map out the quantities into the options bar
+  const quantList = quantArray.map((quantity, index) => {
+    return (
+      <option value={quantity} key={index}>{quantity}</option>
+    )
+  })
+
+  // const quantList = sizeAndQuant(currentSize).map
 
   return (
     <StyledOverviewOptionForm>
         <form>
           <label>
-            <select>
+            <select onChange={(e) => {
+                setSize(currentSize = e.target.value);
+              }}>
               {sizeList}
             </select>
           </label>
           <label>
             <select>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
+              {quantList}
             </select>
           </label>
           <input type="submit" value="Add to Bag"/>
