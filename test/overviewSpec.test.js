7dc1caed5f27170a4636f6b40371908da5828cf4
Nick Kozlarek <kozlarek.nicholas@gmail.com>
2022-08-31 16:43:17 -0600

    Add second test with rendering.

--- test/overviewSpec.test.js
+++ test/overviewSpec.test.js
@@ -9,9 +9,16 @@ const { App } = require('../client/src/app.jsx');
 
 describe('Jest+RTL Workshop', function() {
   const user = userEvent.setup();
-
-  render(<App />)
   test('takes in an object of ratings and the count of each rating and returns a value to the nearest quarter', () => {
     expect(findAverageRating({1: '16', 2: '12', 3: '58', 4: '45', 5: '58'})).toBe("3.50");
+  })
+  test('Ensure that stars do not have any text', () => {
+    render(<App />);
+    return waitFor(() => expect(screen.queryByText(/I'll tell you how great they are after I nap for a bit/)).toBeInTheDocument())
+      .then(() => {
+        expect(screen.getByTestId('stars')).toHaveTextContent('');
+      }).then(() => {
+        expect(screen.getByDisplayValue("Add to Bag")).toHaveTextContent('');
+      })
   })
 })
