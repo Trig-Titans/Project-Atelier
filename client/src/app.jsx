d98b4c5f7e935fd4674e67e368f0222635dcfcae
Nick Kozlarek <kozlarek.nicholas@gmail.com>
2022-09-01 13:59:41 -0600

    Add top side navigator with buttons

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -6,12 +6,49 @@ import QandA from './components/questions/QandA.jsx'
 
 
 export const App = () => {
-  var [currentStyleId, setCurrentStyleId] = useState(0);
-  return  <div >
-           <h1>Front End Capstone Avatar Project</h1>
+  var [currentStyleId, setCurrentStyleId] = useState('221014');
+  function handleScroll (e) {
+    let offsetTop  = document.getElementById(e).offsetTop;
+    window.scrollTo({
+      top: offsetTop-100,
+      behavior: "smooth"
+    });
+  }
+
+  return  (
+          <div>
+            {/* this is the start of the navbar */}
+            <div className="topnav">
+              <h1>Omozan</h1>
+              <div className="topnav-right">
+                <a onClick={() => {
+                  handleScroll("overview")
+                }}>Overview</a>
+                <a onClick={() => {
+                  handleScroll("related-products")
+                }}>Related products</a>
+                <a onClick={() => {
+                  handleScroll("q-and-a")
+                }}>Q and A</a>
+                <a onClick={() => {
+                  handleScroll("reviews")
+                }}>Reviews</a>
+              </div>
+            </div>
+            {/* this is the end of the navbar */}
+            <h1>Front End Capstone Avatar Project</h1>
+            <div id='overview'>
               <Overview currentStyleId={currentStyleId} setCurrentStyleId={setCurrentStyleId}/>
+            </div>
+            <div id='related-products'>
               <RelatedProducts />
+            </div>
+            <div id='q-and-a'>
               <QandA />
+            </div>
+            <div id='reviews'>
               <Reviews />
+            </div>
           </div>
+  )
 }
