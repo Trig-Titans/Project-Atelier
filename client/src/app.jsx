040edafa4f44dd261d6dcd33ba2cf764a122cc8a
Nick Kozlarek <kozlarek.nicholas@gmail.com>
2022-09-01 08:30:25 -0600

    Sets state of sku in app.jsx file so all files have access

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import RelatedProducts from './components/relatedProducts/relatedProductsList.jsx'
 import { Overview } from "./components/overview/GalleryView.jsx"
 import Reviews from "./components/reviews/ReviewsMain.jsx"
@@ -6,10 +6,11 @@ import QandA from './components/questions/QandA.jsx'
 
 
 export const App = () => {
+  var [currentSku, setCurrentSku] = useState(0);
 
   return  <div >
            <h1>Front End Capstone Avatar Project</h1>
-              <Overview />
+              <Overview currentSku={currentSku} setCurrentSku={setCurrentSku}/>
               <RelatedProducts />
               <QandA />
               <Reviews />
