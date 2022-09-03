e8c2ac26b4a7ff1f054962a9da9105855a83718f
Nick Kozlarek <kozlarek.nicholas@gmail.com>
2022-09-03 08:32:31 -0600

    Adds 6 tests for rendering and the form

--- test/overviewSpec.test.js
+++ test/overviewSpec.test.js
@@ -7,7 +7,7 @@ const {default: userEvent} = require('@testing-library/user-event');
 const { render, screen, waitFor } = require('@testing-library/react');
 const { App } = require('../client/src/app.jsx');
 
-describe('Jest+RTL Workshop', function() {
+describe('FEC - Omozan overview tests', function() {
   const user = userEvent.setup();
   beforeEach(() => {
     render(<App />);
@@ -32,12 +32,54 @@ describe('Jest+RTL Workshop', function() {
     })
   })
 
+  // These tests ensure that the form has all of the components and correct number of selections
   it('should not have a size selected on render', () => {
     return waitFor(() => expect(screen.queryByText(/I'll tell/)).toBeInTheDocument())
     .then(() => {
       expect(screen.getByRole('option', { name: '-- select a size --' }).selected).toBe(true)
     })
   })
+
+  it('should display the correct number of options', () => {
+    return waitFor(() => expect(screen.queryByText(/I'll tell/)).toBeInTheDocument())
+    .then(() => {
+      expect(screen.getByTestId('size-options').children.length).toBe(7)
+    });
+  });
+
+  it('should display the correct number of quantities', () => {
+    return waitFor(() => expect(screen.queryByText(/I'll tell/)).toBeInTheDocument())
+    .then(() => {
+      expect(screen.getByTestId('quant-options').children.length).toBe(8)
+    });
+  });
+
+  it('should change the background color to red when clicking add to bag without a size selected', () => {
+    return waitFor(() => expect(screen.queryByText(/I'll tell/)).toBeInTheDocument())
+    .then(() => {
+      return user.click(screen.getByTestId('add-to-bag'))
+    }).then(() => {
+      expect(screen.getByTestId('size-options')).toHaveStyle(`backgroundColor: d107079b`);
+    });
+  });
+
+  it('should change the size with the size select dropdown', () => {
+    return waitFor(() => expect(screen.queryByText(/I'll tell/)).toBeInTheDocument())
+    .then(() => {
+      return user.selectOptions(screen.getByTestId('size-options'), screen.getByRole('option', {name: 'S'}))
+    }).then(() => {
+      expect(screen.getByTestId('size-options')).toHaveTextContent(/S/);
+    });
+  });
+
+  it('should change the color of the star when the favorite button is clicked', () => {
+    return waitFor(() => expect(screen.queryByText(/I'll tell/)).toBeInTheDocument())
+    .then(() => {
+      return user.click(screen.getByTestId('add-to-bag'))
+    }).then(() => {
+      expect(screen.getByTestId('size-options')).toHaveStyle(`backgroundColor: d107079b`);
+    });
+  });
 
   it('should change the style when clicked', () => {
     var currentStyle = '';
@@ -60,4 +102,26 @@ describe('Jest+RTL Workshop', function() {
         expect(currentStyle).toBe(screen.getByTestId('carousel').children[0]);
       })
   })
+
+  it('should display the description upon rendering', () => {
+    return waitFor(() => expect(screen.queryByText(/I'll tell/)).toBeInTheDocument())
+      .then(() => {
+        expect(screen.queryByTestId('description')).toHaveTextContent(/after I nap for a bit/)
+      });
+  });
+
+  it('should display the facts upon rendering', () => {
+    return waitFor(() => expect(screen.queryByText(/I'll tell/)).toBeInTheDocument())
+      .then(() => {
+        expect(screen.queryByTestId('facts').children.length).toBe(2)
+      });
+  });
+
+  it('should display the facts upon rendering', () => {
+    return waitFor(() => expect(screen.queryByText(/I'll tell/)).toBeInTheDocument())
+      .then(() => {
+        expect(screen.queryByTestId('facts')).children.length.toBe(2)
+      });
+  });
+
 })
