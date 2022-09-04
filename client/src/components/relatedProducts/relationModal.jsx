23387958b6080ec4ad3d8c4797e6390001e89ff9
Christopher Garcia <chrisgood2go@gmail.com>
2022-09-03 22:28:14 -0400

    create variables and start building table

--- client/src/components/relatedProducts/relationModal.jsx
+++ client/src/components/relatedProducts/relationModal.jsx
@@ -30,19 +30,73 @@ import {FaCheck} from '@fortawesome/free-solid-svg-icons'
   box-shadow: 0 5px 20px 0 rgba(0, 0, 0, 0.04);
   `
 const RelationModal = (props) => {
+
+let main = props.productData.info;
+let related = props.accumulatedProductData.filter(product => product.info.id === props.productCardClickedOn)[0].info;
+let mainName = main.name;
+let relatedName = related.name;
+let mainFeatures = main.features.map(feature => `${feature.value} ${feature.feature}`);
+let relatedFeatures = related.features.map(feature => `${feature.value} ${feature.feature}`);
+let sameFeatures = mainFeatures.filter(feature => relatedFeatures.indexOf(feature) > -1);
+let differentFeatures = mainFeatures.concat(relatedFeatures).filter(feature => sameFeatures.indexOf(feature) === -1);
+let totalFeatures = sameFeatures.concat(differentFeatures);
+let whereToCheck = totalFeatures.map(feature => {
+  return sameFeatures.indexOf(feature) > -1?
+    'both' : mainFeatures.indexOf(feature) > -1 ?
+      mainName : relatedName;
+});
+let table = [
+            <thead>
+              <tr>
+                <td>{mainName}</td>
+                <td></td>
+                <td>{relatedName}</td>
+              </tr>
+            </thead>
+            ];
+
   return (
     <div>
        <DarkBackGround onClick={()=>{
 
-        console.log('dark background clicked')
+        console.log('dark background clicked', mainFeatures, relatedFeatures, sameFeatures, differentFeatures, totalFeatures, whereToCheck)
         props.setModalOpen(false) }
        }
-        // props.setIsOpen(false) }
         />
     <Centered>
       <Modal>
-        <span>{props.productData.info.name}</span>
-        <span>{props.accumulatedProductData.filter(product => product.info.id === props.productCardClickedOn)[0].info.name}</span>
+
+         <table>
+
+          {table.concat(whereToCheck.map((location, index) => {
+            return location === 'both' ?
+              (
+                <tbody key={index} >
+                  <tr>
+                  <td>{'x'}</td>
+                  <td>{totalFeatures[index]}</td>
+                  <td>{'x'}</td>
+                  </tr>
+                </tbody>
+
+              ) : location === mainName ?
+                (
+                  <tr key={index} >
+                    <td>{'x'}</td>
+                    <td>{totalFeatures[index]}</td>
+                    <td></td>
+                  </tr>
+                )
+                  :  (
+                    <tr key={index} >
+                      <td></td>
+                      <td>{totalFeatures[index]}</td>
+                      <td>{'x'}</td>
+                    </tr>
+
+                    )
+          }))}
+        </table>
       </Modal>
     </Centered>
     </div>
